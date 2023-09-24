9475176a607178017f6952557797ef3acb0f7822
YokoyamaGen <g.yokoyama.0616@gmail.com>
2023-09-24 20:54:54 +0900

    Rubocopによる指摘事項を修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,9 @@
 Product.create!(
   [
     {
-      name: 'マスタリングTCP/IP　入門編（第6版',
+      name: 'マスタリングTCP/IP　入門編',
       price: 2420,
-      description: 'TCP/IP解説書の決定版！　時代の変化によるトピックを加え内容を刷新！　本書は、ベストセラーの『マスタリングTCP/IP 入門編』を時代の変化に即したトピックを加え、内容を刷新した第6版として発行するものです。豊富な脚注と図版・イラストを用いたわかりやすい解説により、TCP/IPの基本をしっかりと学ぶことができます。プロトコル、インターネット、ネットワークについての理解を深める最初の一歩として活用ください。',
+      description: '豊富な脚注と図版・イラストを用いたわかりやすい解説により、TCP/IPの基本をしっかりと学ぶことができます。',
       category: 'book',
       image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join('app/assets/images/tcp_ip.jpg')),
                                                     filename: 'tcp_ip')
@@ -13,16 +13,16 @@ Product.create!(
     {
       name: 'Webを支える技術 ―― HTTP，URI，HTML，そしてREST WEB+DB PRESS plus',
       price: 2827,
-      description: 'Webは誕生から20年で爆発的な普及を果たし，17億人のユーザと2億台のサーバを抱える巨大システムへと成長しました。Webがここまで成功した秘密は，その設計思想，いわゆるアーキテクチャにあります。Webのアーキテクチャ，そしてHTTP，URI，HTMLといったWebを支える技術は，Webがどんなに巨大化しても対応できるように設計されていたのです。私たちが作る個々のWebサービスも，Webのアーキテクチャにのっとることで成功へとつながります。Webのアーキテクチャに正しく適応したWebサービスは，情報が整理され，ユーザの使い勝手が向上し，ほかのサービスと連携しやすくなり，将来的な拡張性が確保されるからです。本書のテーマは，Webサービスの実践的な設計です。まずHTTPやURI，HTMLなどの仕様を歴史や設計思想を織り交ぜて解説します。そしてWebサービスにおける設計課題，たとえば望ましいURI，HTTPメソッドの使い分け，クライアントとサーバの役割分担，設計プロセスなどについて，現時点のベストプラクティスを紹介します。',
+      description: 'Webは誕生から20年で爆発的な普及を果たし，17億人のユーザと2億台のサーバを抱える巨大システムへと成長しました。Webがここまで成功した秘密は，その設計思想，いわゆるアーキテクチャにあります。',
       category: 'book',
       image: ActiveStorage::Blob.create_and_upload!(
-        io: File.open(Rails.root.join('app/assets/images/technology_supports_web.jpg')), filename: 'technology_supports_web'
+        io: File.open(Rails.root.join('app/assets/images/technology_supports_web.jpg')), filename: 'supports_web'
       )
     },
     {
       name: '［試して理解］Linuxのしくみ　―実験と図解で学ぶOS、仮想マシン、コンテナの基礎知識【増補改訂版】',
       price: 3520,
-      description: 'ITシステムやソフトウェアの基盤OSとして幅広く使われているLinux。エンジニアとしてLinuxに関する知識はいまや必須とも言えますが、あなたはそのしくみや動作を具体的にイメージすることができるでしょうか。本書では、Linux OS における、プロセス管理、プロセススケジューラ、メモリ管理、記憶階層、ファイルシステム、記憶階層、そして仮想化機能、コンテナなど、OS とハードウェアに関するしくみがどのように動くのか、実験とその結果を示す豊富なグラフや図解を用いてわかりやすく解説します。改訂に際しては全面フルカラー化。グラフや図解がさらにわかりやすくなり、ソースコードはC言語から、Go言語とPythonにアップデートしています。さらに仮想化、コンテナなどの章が加わりました。今どきのLinuxのしくみを本書でしっかり理解しておきましょう。',
+      description: 'エンジニアとしてLinuxに関する知識はいまや必須とも言えますが、あなたはそのしくみや動作を具体的にイメージすることができるでしょうか。',
       category: 'book',
       image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join('app/assets/images/linux.jpg')),
                                                     filename: 'linux')
@@ -30,10 +30,10 @@ Product.create!(
     {
       name: 'プリンシプル オブ プログラミング 3年目までに身につけたい 一生役立つ101の原理原則',
       price: 2420,
-      description: '一通りプログラミングができるようになった。しかし、読みにくい、遅い、頻繁にエラーが発生する、書いたコードを修正すると動かなくなる等々、なかなか「よいコード」を書けないとお悩みではありませんか？ 本書は、よいコードを書く上で指針となる前提・原則・思想、つまり「プリンシプル」を解説するプログラミングスキル改善書です。初心者向けの書籍では絶対に説明しない、古今東西のプログラマーの知恵をこの一冊に凝縮しました！',
+      description: '一通りプログラミングができるようになった。しかし、読みにくい、遅い、頻繁にエラーが発生する、書いたコードを修正すると動かなくなる等々、なかなか「よいコード」を書けないとお悩みではありませんか？',
       category: 'book',
       image: ActiveStorage::Blob.create_and_upload!(
-        io: File.open(Rails.root.join('app/assets/images/principles_of_programming.jpg')), filename: 'principles_of_programming'
+        io: File.open(Rails.root.join('app/assets/images/principles_of_programming.jpg')), filename: 'principles'
       )
     },
     {
