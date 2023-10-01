b625632b0677d36411aad1da6272c914477ee6bf
YokoyamaGen <g.yokoyama.0616@gmail.com>
2023-10-01 18:44:14 +0900

    商品の画像が登録されていなかった場合、デフォルト画像が表示されるように対応

---
+++ app/decorators/product_decorator.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class ProductDecorator < ApplicationDecorator
+  delegate_all
+
+  def display_image
+    if object.image.attached?
+      object.image
+    else
+      'noimage'
+    end
+  end
+
+  # Define presentation-specific methods here. Helpers are accessed through
+  # `helpers` (aka `h`). You can override attributes, for example:
+  #
+  #   def created_at
+  #     helpers.content_tag :span, class: 'time' do
+  #       object.created_at.strftime("%a %m/%d/%y")
+  #     end
+  #   end
+end
