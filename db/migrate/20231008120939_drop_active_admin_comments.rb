f60ccd0b14b52ae6c0b273b22de239d47ab359c1
YokoyamaGen <g.yokoyama.0616@gmail.com>
2023-10-08 21:28:44 +0900

    Rubocopによる指摘事項を修正

--- db/migrate/20231008120939_drop_active_admin_comments.rb
+++ db/migrate/20231008120939_drop_active_admin_comments.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 class DropActiveAdminComments < ActiveRecord::Migration[7.0]
   def change
-    drop_table :active_admin_comments
+    drop_table :active_admin_comments do
+    end
   end
 end
