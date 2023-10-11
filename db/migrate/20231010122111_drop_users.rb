fba450d4855cbb9bc5531f7fc54b57799b165bec
YokoyamaGen <g.yokoyama.0616@gmail.com>
2023-10-12 08:41:24 +0900

    Userモデルを使用せずに実装するため、Userモデルを削除

---
+++ db/migrate/20231010122111_drop_users.rb
@@ -0,0 +1,10 @@
+class DropUsers < ActiveRecord::Migration[7.0]
+  def change
+    drop_table :users do |t|
+      t.string :ip_address, null: false
+      t.string :browser, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_08_120939) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_10_122111) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,13 +43,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_08_120939) do
   end
 
   create_table "carts", force: :cascade do |t|
-    t.bigint "user_id", null: false
     t.bigint "product_id", null: false
     t.integer "count", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["product_id"], name: "index_carts_on_product_id"
-    t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -69,15 +67,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_08_120939) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "ip_address", null: false
-    t.string "browser", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "carts", "products"
-  add_foreign_key "carts", "users"
 end
