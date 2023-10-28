b79af631794e52d7d5f942f6e7efe6c70443541c
YokoyamaGen <g.yokoyama.0616@gmail.com>
2023-10-28 10:41:59 +0900

    バリデーションにひかかった場合、画面側に背景色が赤色で囲まれて、エラーメッセージが出るように修正

--- app/controllers/products/carts_controller.rb
+++ app/controllers/products/carts_controller.rb
@@ -8,7 +8,7 @@ module Products
       if current_cart.add_item(product_id: @product.id, quantity: params[:cart][:quantity])
         redirect_to request.referer, flash: { success: 'カートに商品を追加しました' }
       else
-        flash[:alert] = current_item.errors.full_messages.join("\n")
+        flash[:danger] = current_item.errors.full_messages.join("\n")
         redirect_to request.referer
       end
     end
