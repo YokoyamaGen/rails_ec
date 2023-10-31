5ae2c4f127b5c05106cf81694b686e46ea604596
YokoyamaGen <g.yokoyama.0616@gmail.com>
2023-10-31 22:54:25 +0900

    mailcatcherをローカル環境で使用できるように実装

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,10 @@ Rails.application.configure do
     config.action_controller.perform_caching = false
 
     config.cache_store = :null_store
+
+    config.action_mailer.delivery_method = :smtp
+
+    config.action_mailer.smtp_settings = { address: 'mailcatcher', port: 1025 }
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
