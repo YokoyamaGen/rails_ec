2d532729ba14d3c6c487a4568c6ce17e18697fd1
YokoyamaGen <g.yokoyama.0616@gmail.com>
2025-04-27 15:58:59 +0900

    Rubocopによる指摘事項を修正

--- db/migrate/20250427064023_add_index_to_checkouts_created_at.rb
+++ db/migrate/20250427064023_add_index_to_checkouts_created_at.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class AddIndexToCheckoutsCreatedAt < ActiveRecord::Migration[7.0]
   def change
-    add_index :checkouts, [:cart_id, :created_at]
+    add_index :checkouts, %i[cart_id created_at]
   end
 end
